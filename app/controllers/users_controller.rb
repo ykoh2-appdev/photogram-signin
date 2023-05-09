5807d258d2c078e5e74c9bd622c39de24581dbcd
Yoon Koh <ykoh2@chicagobooth.edu>
2023-05-09 20:56:57 +0000

    completed authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,8 @@ class UsersController < ApplicationController
     save_status = user.save
 
     if save_status == true
+      session.store(:user_id, user.id)
+
       redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
     else
       redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
@@ -53,4 +55,38 @@ class UsersController < ApplicationController
     render({:template => "/users/signup_form.html.erb"})
   end
 
+  def new_session_form
+    render({:template => "/users/signin_form.html.erb"})
+  end
+
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!" })
+  end
+
+  def authenticate
+     # get the username from params
+     un = params.fetch("input_username")
+     # get the password from params
+     pw = params.fetch("input_password")
+
+     # look up the recomrd from the db matching username
+     user = User.where({ :username => un }).at(0)
+     # if no record, redirect back to sign in form
+     if user == nil 
+      redirect_to("/user_sign_in", { :alert => "No one by that name 'round these parts" })      
+     else
+      # if there is a record, check to see if password matches
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+
+
+      # if not,redirect back to sign in form
+        redirect_to("/", { :notice => "Welcome back, " + user.username + "!" })
+      else
+        redirect_to("/user_sign_in", { :alert => "Nice try, sucker" })
+      end
+     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 
   get("/user_sign_up", {:controller => "users", :action => "new_registration_form"})
 
+  get("/user_sign_out", {:controller => "users", :action => "toast_cookies"})
+
+  get("/user_sign_in", {:controller => "users", :action => "new_session_form"})
+
+  post("/verify_credentials", {:controller => "users", :action => "authenticate"})
   # CREATE
   get("/insert_user_record", {:controller => "users", :action => "create" })
 
